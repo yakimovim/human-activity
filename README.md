ee4f03e53ebfed1475d05c3c090f35951f1f9da6
Ivan Iakimov <edlinsoft@gmail.com>
2017-12-12 11:42:47 +0300

    Cleaned data set

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# human-activity
-Data Cleaning Of Human Activity Recognition Using Smartphones Data Set
+# Data Cleaning Of Human Activity Recognition Using Smartphones Data Set
+
+The repository contains an initial data from 'Human Activity Recognition Using Smartphones Data Set', script to clean the data, file with resilting cleaned data set, code book explaining content of this file.
+
+## Content
+
+* 'UCI HAR Dataset' - folder with initial data set.
+* 'run_analysis.R' - script written in R language to clean the data and to create resulting data set in 'averaged_data_by_subject_and_activity.txt' file.
+* 'averaged_data_by_subject_and_activity.txt' - resulting data set.
+* 'CodeBook.md' - description of the content of the 'averaged_data_by_subject_and_activity.txt' data set.
+
+## How the R script works
+
+You must be aware that the script required 'dplyr' R package installed.
+
+* First of all it read information about all features and all activities into 'features' and 'activity_labels' frames.
+* Then it extracts only names of features containing 'mean(' or 'std(' into 'mean_std_features' vector.
+* After that the script starts reading training data set.
+* It reads information about subjects participating in the training into 'train_subject' frame.
+* It reads identifiers of activities into 'train_labels', merges it with 'activity_labels' to get names of activities of each observation. The information is stored into 'train_labels' frame.
+* It reads variables observations into 'train_data' frame. Then it leaves only columns from 'mean_std_features' vector. The result is stored in the 'train_data' frame.
+* It merges 'train_subject', 'train_labels' and 'train_data' frames using 'cbind' to assign subjects and activities to the observations. The result is stored into the 'train_data' frame
+* After that the same process is repeated for the testing data set. In the end we have 'test_data' frame with the same columns as in the 'train_data' frame.
+* Then we merge training and testing data together into 'all_data' frame.
+* We group this frame by subject and activity into 'all_data_by_subject_and_activity' frame.
+* After that we calculate average value (mean) for each group using 'summarise_each' function. The result is stored into 'averaged_data_by_subject_and_activity' frame.
+* And we clean names of variables/columns by removing '(' and ')' symbols.
+* In the end we store the result into 'averaged_data_by_subject_and_activity.txt' file using 'write.table' function.
--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 # Read information about activities names
 
 activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", 
@@ -35,13 +37,16 @@ train_labels <- read.table("UCI HAR Dataset/train/y_train.txt",
                           sep = " ", 
                           colClasses = c("numeric"),
                           col.names = c("activityId"))
+train_labels <- merge(train_labels, activity_labels, by.x = "activityId", by.y = "activityId")
+train_labels <- data.frame(train_labels[, "activityName"])
+names(train_labels) <- "activityName"
 
 # Read data
 
-train_data <- strsplit(readLines("UCI HAR Dataset/train/X_train.txt"), split = " ")
-train_data <- lapply(train_data, function(v) { v[v != ""] })
-train_data <- lapply(train_data, as.numeric)
-train_data <- data.frame(matrix(unlist(train_data), nrow = length(train_data), byrow=TRUE))
+train_data <- read.table("UCI HAR Dataset/train/X_train.txt", 
+                         header = FALSE, 
+                         strip.white = TRUE, 
+                         colClasses = "numeric")
 names(train_data) <- features$featureName
 
 # Choose only columns for mean and standard deviation
@@ -69,13 +74,16 @@ test_labels <- read.table("UCI HAR Dataset/test/y_test.txt",
                          sep = " ", 
                          colClasses = c("numeric"),
                          col.names = c("activityId"))
+test_labels <- merge(test_labels, activity_labels, by.x = "activityId", by.y = "activityId")
+test_labels <- data.frame(test_labels[, "activityName"])
+names(test_labels) <- "activityName"
 
 # Read data
 
-test_data <- strsplit(readLines("UCI HAR Dataset/test/X_test.txt"), split = " ")
-test_data <- lapply(test_data, function(v) { v[v != ""] })
-test_data <- lapply(test_data, as.numeric)
-test_data <- data.frame(matrix(unlist(test_data), nrow = length(test_data), byrow=TRUE))
+test_data <- read.table("UCI HAR Dataset/test/X_test.txt", 
+                        header = FALSE, 
+                        strip.white = TRUE, 
+                        colClasses = "numeric")
 names(test_data) <- features$featureName
 
 # Choose only columns for mean and standard deviation
@@ -87,3 +95,19 @@ test_data <- test_data[,mean_std_features]
 test_data <- cbind(test_subject, test_labels, test_data)
 
 all_data <- rbind(test_data, train_data)
+
+# Group data by subject and activity
+
+all_data_by_subject_and_activity <- group_by(all_data, subjectId, activityName)
+
+# Calculate averages of variables
+
+averaged_data_by_subject_and_activity <- summarise_each(all_data_by_subject_and_activity, funs(mean))
+
+# Clean variable names from brackets
+
+names(averaged_data_by_subject_and_activity) <- gsub("\\(|\\)", "", names(averaged_data_by_subject_and_activity))
+
+# Write result to a file
+
+write.table(averaged_data_by_subject_and_activity, file = "averaged_data_by_subject_and_activity.txt", row.names = FALSE)
